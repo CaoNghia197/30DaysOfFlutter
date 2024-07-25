2a3820e1e853a87b607dafd255c561db6034ba65
TNghia <caonghia97@gmail.com>
2024-07-25 13:38:23 +0700

    2024/0724

--- day02_tip_caculator/lib/main.dart
+++ day02_tip_caculator/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final controller = TextEditingController();
   final _selection = [true, false, false];
+  String? tip;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +26,15 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              if (tip != null)
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    tip!,
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
               const Text('Total Amount'),
               SizedBox(
                 width: 70,
@@ -49,11 +59,14 @@ class _MyAppState extends State<MyApp> {
                 ),
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: calculateTip,
                 style: ButtonStyle(
                     backgroundColor: MaterialStateColor.resolveWith(
                         (states) => Colors.green)),
-                child: const Text('Calculator Tip', style: TextStyle(color: Colors.white),),
+                child: const Text(
+                  'Calculator Tip',
+                  style: TextStyle(color: Colors.white),
+                ),
               )
             ],
           ),
@@ -67,6 +80,16 @@ class _MyAppState extends State<MyApp> {
       for (int i = 0; i < _selection.length; i++) {
         _selection[i] = selectionIndex == i;
       }
+    });
+  }
+
+  void calculateTip() {
+    final totalAmount = double.parse(controller.text);
+    final selectedIndex = _selection.indexWhere((element) => element);
+    final tipPercentage = [0.1, 0.15, 0.2][selectedIndex];
+    final tipTotal = (totalAmount * tipPercentage).toStringAsFixed(2);
+    setState(() {
+      tip = '\$$tipTotal';
     });
   }
 }
